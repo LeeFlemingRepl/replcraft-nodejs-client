22452d95a559ffdd70b9b9abb8abf595c550a2ce
LeeFlemingRepl <102989413+LeeFlemingRepl@users.noreply.github.com>
2022-05-23 19:28:46 -0700

    Add target_index and amount to moveItem

--- replcraft.js
+++ replcraft.js
@@ -412,15 +412,17 @@ class Client extends EventEmitter {
    * @param {number} target_x the x coordinate of the source container (container relative)
    * @param {number} target_y the y coordinate of the source container (container relative)
    * @param {number} target_z the z coordinate of the source container (container relative)
+   * @param {number|null} target_index the target index in the destination container, or any if null
+   * @param {number|null} amount the amount of items to move, or all if null
    * @return {Promise}
    * @throws {CraftError}
    */
-  moveItem(index, source_x, source_y, source_z, target_x, target_y, target_z) {
+  moveItem(index, source_x, source_y, source_z, target_x, target_y, target_z, target_index=null, amount=null) {
     return this.request({
       action: 'move_item',
-      index,
-      source_x, source_y, source_z,
-      target_x, target_y, target_z
+      amount,
+      index, source_x, source_y, source_z,
+      target_index, target_x, target_y, target_z
     }).then(() => {});
   }
   
